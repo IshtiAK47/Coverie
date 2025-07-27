fdd838a637c44b468adcdd59debfa3856ff317c8
Ishtiak Mahmood <m64445.0@gmail.com>
2025-07-27 21:58:06 +0600

    Update cover-page-preview.tsx

--- src/components/cover-page-preview.tsx
+++ src/components/cover-page-preview.tsx
@@ -53,7 +53,7 @@ export default function CoverPagePreview({ form }: { form: UseFormReturn<CoverPa
         <header className="flex flex-col items-center text-center mb-12">
           <div className="mb-4">
              <Image
-                src="https://github.com/IshtiAK47/Coverie/blob/master/public/logo.png"
+                src="https://raw.githubusercontent.com/IshtiAK47/Coverie/refs/heads/master/public/logo.png"
                 alt="University Logo"
                 width={100}
                 height={100}
